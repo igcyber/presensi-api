38df93dbdc80ae683354192ff701204356ac5b62
Murabito I <irfan.persona3fes@gmail.com>
2025-12-03 01:54:05 +0800

    Fix Seeder Kantor

--- database/seeders/kantor_seeder.ts
+++ database/seeders/kantor_seeder.ts
@@ -12,10 +12,10 @@ export default class KantorSeeder extends BaseSeeder {
 				{
 					nama: 'Dinas Komunikasi dan Informatika Kabupaten Kutai Kartanegara',
 					alamat: 'Jalan Pahlawan No. 1 Bukit Biru, Timbau, Kec. Tenggarong, Kabupaten Kutai Kartanegara, Kalimantan Timur 75511',
-					lat: '-0.4191422207962125',
-					long: '117.16900524248992',
+					lat: '-0.4569253',
+					long: '117.0019486',
 					deskripsi: 'Diskominfo Kab. Kukar',
-					radius_limit: 100,
+					radius_limit: 20,
 					except_user: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ],
 					jam_masuk: '07:30:00',
 					jam_pulang: '16:30:00'
