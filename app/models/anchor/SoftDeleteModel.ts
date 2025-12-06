56cfdf89f3139626eb0764e8b222b055d0cf314d
Murabito I <irfan.persona3fes@gmail.com>
2025-12-06 23:14:54 +0800

    Hidden DeletedAt column

--- app/models/anchor/SoftDeleteModel.ts
+++ app/models/anchor/SoftDeleteModel.ts
@@ -1,8 +1,13 @@
 import { compose } from '@adonisjs/core/helpers'
+import { column } from '@adonisjs/lucid/orm'
+
 import { SoftDeletes } from 'adonis-lucid-soft-deletes'
 import BaseModel from '#models/anchor/BaseModel'
 
 // @ts-ignore
 export default class SoftDeleteModel extends compose(BaseModel, SoftDeletes) {
 	static softDeleteColumn = 'deleted_at'
+
+	@column({ serializeAs: null })
+	declare deleted_at: string
 }
